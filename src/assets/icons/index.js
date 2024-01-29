917996df2496e2b93fb369fbfb133d03cdbb1266
Abbos1506 <abbosomonboyev686@gmail.com>
2024-01-29 15:52:02 +0500

    change login

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,3 +4,4 @@ export { default as GroupIcon } from "./GroupIcon";
 export { default as MoneyIcon } from "./MoneyIcon";
 export { default as PayIcon1 } from "./PayIcon1";
 export { default as PayIcon2 } from "./PayIcon2";
+export { default as LoginImg1 } from "./LoginImg1";
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,63 +1,98 @@
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Form, Input, message } from 'antd';
 import { usePostRequest } from "../../hooks/request";
-import { Urls } from "../../utils/urls";
 import { UserContext } from "../../context/userContext";
 import { useContext } from "react";
+import { Urls } from "../../utils/urls";
+import { message, Form, Input, Button } from "antd";
+import Title from "antd/es/typography/Title";
+import LoginLogo from "../../assets/image/LoginLogo.png";
+import { LoginImg1 } from "../../assets/icons";
+
 const LoginPage = () => {
   const loginPost = usePostRequest({ url: Urls.auth.login });
-  
-   const { setTokens } = useContext(UserContext);
-  const handlyFinish = async (values) => {
-    console.log(values)
-    const { success, response, error } = await loginPost.request({
-        data: values,
-    });
-  if (success && !!response) {
-    const { accessToken, refreshToken } = response.data;
-    localStorage.setItem("accessToken",accessToken)
-    localStorage.setItem("refreshToken", refreshToken)
-    setTokens(accessToken,refreshToken)
-    } else {
-        message.error(error);
-    }
-};
-  return (
-    <div className="Lopinparent">
-      <div className="Login">
-      <h1 className="Login__title">Tizimga kirish</h1>
-      <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{ remember: true }}
-      onFinish={handlyFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[{ required: true, message: 'Please input your Login!' }]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Login" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your !' }]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Parol"
-        />
-      </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-        Kirish
-        </Button>
-      </Form.Item>
+  const { setTokens } = useContext(UserContext);
+  const handlyFinish = async (values) => {
+    console.log(values);
+    const { success, response, error } = await loginPost.request({
+      data: values,
+    });
+    if (success && !!response) {
+      const { accessToken, refreshToken } = response.data;
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      setTokens(accessToken, refreshToken);
+    } else {
+      message.error(error);
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-page__logo">
+        <img src={LoginLogo} alt="" />
+      </div>
+      <div className="login-page__image">
+        <LoginImg1 />
+      </div>
+      <Form
+        className="login-card"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={handlyFinish}
+        autoComplete="off"
+      >
+        <Title className="login-card__title">Tizimga kirish</Title>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          className="input-password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Kirish
+          </Button>
+        </Form.Item>
     </Form>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
